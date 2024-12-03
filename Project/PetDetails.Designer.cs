24cb34e10cd1d3256c849d60161fdff0cb198f9c
Pavch0 <pavel.papazov.highschool@buditel.bg>
2024-12-03 23:55:51 +0200

    project added

---
+++ Project/PetDetails.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Vet_Clinic
+{
+    partial class PetDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label9 = new Label();
+            button1 = new Button();
+            label10 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Javanese Text", 30F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(300, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(291, 114);
+            label1.TabIndex = 0;
+            label1.Text = "Pet's name";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(49, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(179, 107);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(551, 360);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(250, 27);
+            textBox7.TabIndex = 22;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(551, 293);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(250, 27);
+            textBox6.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(551, 240);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(250, 27);
+            textBox5.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(551, 174);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(250, 27);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(188, 334);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(248, 118);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(186, 286);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(186, 174);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 27);
+            textBox1.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("SimSun-ExtG", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(186, 225);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 22);
+            dateTimePicker1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(455, 359);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 26);
+            label8.TabIndex = 26;
+            label8.Text = "Favorite";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(455, 292);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 26);
+            label7.TabIndex = 25;
+            label7.Text = "Status";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(458, 239);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 26);
+            label6.TabIndex = 24;
+            label6.Text = "Color";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(456, 174);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 26);
+            label5.TabIndex = 23;
+            label5.Text = "Weight";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(24, 335);
+            label4.Name = "label4";
+            label4.Size = new Size(158, 26);
+            label4.TabIndex = 21;
+            label4.Text = "Medical History";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(88, 287);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 26);
+            label3.TabIndex = 19;
+            label3.Text = "Breed";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(71, 228);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 26);
+            label2.TabIndex = 16;
+            label2.Text = "Birthday";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(88, 175);
+            label9.Name = "label9";
+            label9.Size = new Size(57, 26);
+            label9.TabIndex = 14;
+            label9.Text = "Type";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.Font = new Font("Sitka Text", 16.1999989F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(663, 442);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 49);
+            button1.TabIndex = 27;
+            button1.Text = "Adopt";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(455, 388);
+            label10.Name = "label10";
+            label10.Size = new Size(47, 26);
+            label10.TabIndex = 28;
+            label10.Text = "Toy";
+            // 
+            // PetDetails
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DeepSkyBlue;
+            ClientSize = new Size(882, 503);
+            Controls.Add(label10);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label9);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "PetDetails";
+            Text = "PetDetails";
+            Load += FillPetInfo;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label9;
+        private Button button1;
+        private Label label10;
+    }
+}
